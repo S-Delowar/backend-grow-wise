65658360ca463b8d8dd3db1da5fa57c63e9bae4b
S-Delowar <sayed.buet97@gmail.com>
2026-01-25 11:22:47 +0600

    update requirements

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-#pifz_hqa1_vqjs-^xx494@jj_on26t14$gn56@s9hxmgkdv3*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
